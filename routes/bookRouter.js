f958dcddafa8e68de7fb87148902434fb5d4c09a
MDAkramSiddiqui <you@example.com>
2020-03-18 00:12:03 +0530

    feat(app): Addition of Put patch and delete apis

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,4 @@
+//es lint special linting options for this file or even a specific line can be added via es-lint comments
 const express = require('express');
 
 const bookRoutes = function(Book) {
@@ -45,8 +46,29 @@ const bookRoutes = function(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
-      res.json(book);
+      book.save(err => {
+        if(err) return res.send(err);
+        else return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if(req.body._id) delete req.body._id;
+      Object.entries(req.body).forEach(item => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      book.save(err => {
+        if(err) return res.send(err);
+        else return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove(err => {
+        if(err) return res.send(err);
+        return res.sendStatus(204);
+      });
     });
   
   return bookRouter;
